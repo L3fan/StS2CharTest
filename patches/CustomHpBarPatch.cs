5f9c3c5a6947dccd47bce630378da7f273dcf662
Schmidtfe <116957574+Schmidtfe@users.noreply.github.com>
2026-03-29 14:22:12 +0200

    Fixed issue with Embers HP Bar indicator not properly rendering when lethal

--- patches/CustomHpBarPatch.cs
+++ patches/CustomHpBarPatch.cs
@@ -40,7 +40,7 @@ internal class CustomHpBarPatch
             int embersDamageNextTurn = emberPower != null ? emberPower.CalculateTotalDamage() : 0;
             int totalDamageNextTurn = poisonDamageNextTurn + embersDamageNextTurn;
 
-            float num1 = GetFgWidth(____creature, GetMaxFgWidth(____hpForegroundContainer, ____expectedMaxFgWidth), GetMaxFgWidth(____hpForegroundContainer, ____expectedMaxFgWidth)) - GetMaxFgWidth(____hpForegroundContainer, ____expectedMaxFgWidth);
+            float num1 = GetFgWidth(____creature, ____creature.CurrentHp, GetMaxFgWidth(____hpForegroundContainer, ____expectedMaxFgWidth)) - GetMaxFgWidth(____hpForegroundContainer, ____expectedMaxFgWidth);
             if (____creature.HasPower<EmbersPower>())
             {
                 _embersForeground.Visible = true;
@@ -53,18 +53,19 @@ internal class CustomHpBarPatch
                         _embersForeground.OffsetLeft = 0.0f;
                         _embersForeground.OffsetRight = num1;
                         ____hpForeground.Visible = false;
+                        MainFile.Logger.Info("Embers Offset: " + _embersForeground.OffsetRight);
                     }
                     else
                     {
                         float fgWidth = GetFgWidth(____creature, ____creature.CurrentHp - totalDamageNextTurn, GetMaxFgWidth(____hpForegroundContainer, ____expectedMaxFgWidth));
                         ____hpForeground.OffsetRight = fgWidth - GetMaxFgWidth(____hpForegroundContainer, ____expectedMaxFgWidth);
-                        MainFile.Logger.Info("HP Foreground Offset: " + _embersForeground.OffsetRight);
+                        //MainFile.Logger.Info("HP Foreground Offset: " + _embersForeground.OffsetRight);
                         ____hpForeground.Visible = true;
                         int patchMarginLeft = ((NinePatchRect) _embersForeground).PatchMarginLeft;
                         _embersForeground.OffsetLeft = Math.Max(0.0f, fgWidth - patchMarginLeft);
                         float fgWidthSansPoison = GetFgWidth(____creature, ____creature.CurrentHp - poisonDamageNextTurn, GetMaxFgWidth(____hpForegroundContainer, ____expectedMaxFgWidth));
                         _embersForeground.OffsetRight = fgWidthSansPoison - GetMaxFgWidth(____hpForegroundContainer, ____expectedMaxFgWidth);
-                        MainFile.Logger.Info("Embers Offset: " + _embersForeground.OffsetRight);
+                        //MainFile.Logger.Info("Embers Offset: " + _embersForeground.OffsetRight);
                     }
                 }
                 else
