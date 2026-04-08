3e5f3e9e9d07e896511734af12fdc224f29a5eeb
Schmidtfe <116957574+Schmidtfe@users.noreply.github.com>
2026-04-08 16:54:39 +0200

    Added new Energy Icon and Spark card

---
+++ Cards/Common/Spark.cs
@@ -0,0 +1,29 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using StS2CharTest.Code.Character;
+
+namespace StS2CharTest.Cards.Common;
+
+[Pool(typeof(CharTestCardPool))]
+public class Spark() : CharTestCard(0, CardType.Skill,
+    CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(2)];
+    public override int CanonicalHeatCost => 3;
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await PlayerCmd.GainEnergy(DynamicVars.Energy.IntValue, Owner.Creature.Player);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Energy.UpgradeValueBy(1);
+    }
+}
--- CustomNodes/CustomEnergyCounter.cs
+++ CustomNodes/CustomEnergyCounter.cs
@@ -1,8 +1,30 @@
-﻿using MegaCrit.Sts2.Core.Nodes.Combat;
+﻿using Godot;
+using MegaCrit.Sts2.Core.Assets;
+using MegaCrit.Sts2.Core.Nodes.Combat;
 
 namespace StS2CharTest.Character;
 
 public partial class CustomEnergyCounter : NEnergyCounter
 {
-    
+    public override void _Ready()
+    {
+        base._Ready();
+        Control Layers = GetNode<Control>("%Layers");
+
+        AddMaterialToChildrenOf(Layers);
+    }
+
+    private void AddMaterialToChildrenOf(Node nodeToCheck)
+    {
+        foreach (var child in nodeToCheck.GetChildren())
+        {
+            if (child.GetType() != typeof(TextureRect))
+            {
+                AddMaterialToChildrenOf(child);
+                continue;
+            }
+            Control controlNode = child as Control;
+            controlNode.Material = PreloadManager.Cache.GetMaterial("res://materials/ui/energy_orb_dark.tres");
+        }
+    }
 }
