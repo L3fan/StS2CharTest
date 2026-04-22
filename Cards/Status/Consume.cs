b646ad8d47ce2e0522f6e5fdbf036752b2e28791
Schmidtfe <116957574+Schmidtfe@users.noreply.github.com>
2026-04-22 14:42:37 +0200

    Added Consume card and DEPRECATED Consumption Power

---
+++ Cards/Status/Consume.cs
@@ -0,0 +1,29 @@
+﻿using BaseLib.Abstracts;
+using BaseLib.Extensions;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+using StS2CharTest.Actions;
+using StS2CharTest.Powers;
+
+namespace StS2CharTest.Cards.Status;
+
+[Pool(typeof(StatusCardPool))]
+public class Consume() : CharTestCard(0, CardType.Status,
+    CardRarity.Status, TargetType.Self)
+{
+    public override int MaxUpgradeLevel => 0;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<ConsumptionPower>(1).WithTooltip("CONSUMPTION")];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        await CommonActions.ApplySelf<ConsumptionPower>(this);
+    }
+}
--- Powers/FlyingPower.cs
+++ Powers/FlyingPower.cs
@@ -2,6 +2,7 @@
 using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -31,7 +32,7 @@ public class FlyingPower : CharTestPowerModel
         return DynamicVars["DamageDecrease"].BaseValue / 100m;
     }
 
-    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    public override async Task AfterPlayerTurnStartLate(PlayerChoiceContext choiceContext, Player player)
     {
         await PowerCmd.Decrement(this);
     }
